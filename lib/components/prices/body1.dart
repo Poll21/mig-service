145ab76de4fd3af5e999677a694703b3a0be9dc0
Poll21 <gol.poll21@gmail.com>
2021-09-27 14:30:44 +0700

    внесение изменений

--- lib/components/prices/body1.dart
+++ lib/components/prices/body1.dart
@@ -13,44 +13,92 @@ class BodiPrise1 extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          Stack(
+            alignment: AlignmentDirectional.bottomCenter,
+            children: [
+              Container(
+                height: size.height * 0.15 + kDefaultPadding * 5,
+                padding: EdgeInsets.only(
+                    left: kDefaultPadding,
+                    top: kDefaultPadding,
+                    right: kDefaultPadding,
+                    bottom: kDefaultPadding * 2),
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(kDefaultRadius),
+                      bottomRight: Radius.circular(kDefaultRadius)),
+                  //boxShadow:
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
 
-            Container(
-              height: 60 + kDefaultPadding * 5,
-              padding: EdgeInsets.only(
-                  left: kDefaultPadding,
-                  top: kDefaultPadding,
-                  right: kDefaultPadding,
-                  bottom: kDefaultPadding * 2),
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(kDefaultRadius),
-                    bottomRight: Radius.circular(kDefaultRadius)),
-                //boxShadow:
+                    Image.asset('assets/tablet.png'),
+                    Center(
+                      child: PriseTitle(title:'РЕМОНТ ПЛАНШЕТОВ',size: size,),
+                    ),
+
+
+                  ],
+                ),
+
+
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
+              IconButton(onPressed:  () {}, icon: Icon(Icons.menu)),
+            ],
+          ),
 
-                Image.asset('assets/tablet.png'),
-                Center(
-                  child: PriseTitle(title:'РЕМОНТ ПЛАНШЕТОВ',size: size,),
-                )
-
-
-
-
-
-
-              ],
-            ),
-
-
-            ),
 
           Expanded(
             flex: 8,
-              child: Container())
+              child: Container(
+                margin: EdgeInsets.only(
+                    left: kDefaultPadding,
+                    top: kDefaultPadding,
+                    right: kDefaultPadding,
+                    bottom: kDefaultPadding * 2),
+                padding: EdgeInsets.only(
+                    left: kDefaultPadding,
+                    top: kDefaultPadding,
+                    right: kDefaultPadding,
+                    bottom: kDefaultPadding * 2),
+                decoration: BoxDecoration(
+                  color: kRextColor.shade300,
+                  borderRadius: BorderRadius.all(
+                       Radius.circular(kDefaultRadius)),
+                  //boxShadow:
+                ),
+                child:
+                  Column (
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: kDefaultPadding),
+                        child: Text('ВНИМНИЕ!!!',
+                          style: TextStyle(color: kTextColor,fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: kDefaultPadding),
+                        child: Text('В прайсе указана минимальная стоимость услуг, без учета стоимости запчастей и сложности монтажа устройства!!!',
+                          style: TextStyle(color: kTextColor,fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: kDefaultPadding),
+                        child: Text('Уточнить стоимость запчастей и сложность монтажа устройства можно связавшись с нами',
+                          style: TextStyle(color: kTextColor,fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+              ))
         ],
       ),
 
@@ -70,13 +118,15 @@ class PriseTitle extends StatelessWidget {
   final String title;
   final Size size;
 
-
   @override
   Widget build(BuildContext context){
+
     return
       Container(
         padding: EdgeInsets.only(
-            left: kDefaultPadding, right: kDefaultPadding),
+            left: kDefaultPadding,
+            right: kDefaultPadding,
+            bottom: kDefaultPadding / 6),
         decoration: BoxDecoration(
           color: kRextColor,
           borderRadius: BorderRadius.all(
@@ -84,10 +134,11 @@ class PriseTitle extends StatelessWidget {
         ),
         child: Text(title,
           style: TextStyle(
-            color: kPrimaryColor,
+            color: kTextColor,
             fontWeight: FontWeight.bold,
-            
+            fontSize: size.width * 0.05,
           ),
+          textAlign: TextAlign.center,
         ),
       );
   }
