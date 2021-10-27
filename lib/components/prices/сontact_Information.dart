78554285d5f7e8f34bfeeebea451c2e538798661
Poll <gol.poll21@gmail.com>
2021-10-27 08:37:09 +0700

    причесать контакты

--- lib/components/prices/сontact_Information.dart
+++ lib/components/prices/сontact_Information.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:migservice/components/size_progect.dart';
 import 'package:migservice/pages/costum_appbar.dart';
 import 'package:migservice/pages/home_screen.dart';
 import '../../costomicons_icons.dart';
@@ -20,13 +21,14 @@ class ContactInf extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroudColor,
-      body: Column(children: [
-        CustomAppBar(
+      body: Column(
+          children: [
+            CustomAppBar(
             icon: Icons.chevron_left,
             pressIcon: () => Navigator.push(context,
                 MaterialPageRoute(builder: (context) => HomeScreen()))),
-        TitlePag(title: 'Контактная информация:'),
-        Container(
+            TitlePag(title: 'Контактная информация:'),
+            Container(
           height: kDefaultRadius *2,
           padding: EdgeInsets.only(top: kDefaultPadding * 2),
           decoration: BoxDecoration(
@@ -35,122 +37,17 @@ class ContactInf extends StatelessWidget {
                   topLeft: Radius.circular(kDefaultRadius * 2),
                   topRight: Radius.circular(kDefaultRadius * 2))),
         ),
-        Expanded(
-          child:Row(
-            children: [
-              Expanded(
-              flex: 1,
-                  child:
-                  Container(
-                    color: kPrimaryColor.withOpacity(0.30),
-                    child:
-                    ListView(
-                      scrollDirection: Axis.vertical,
-                      children: <Widget>[
-                        OperatingMode(kSize: kDesktopSize,),
-                        IconCont(
-                            kSize: kDesktopSize,
-                            kColor: Colors.green[500],
-                            kIcon: Icons.phone,
-                            kText1: '+7 (3852)-210-74-43;',
-                            kText2: '+7-913-210-74-43'),
-                        IconCont(
-                            kSize: kDesktopSize,
-                            kColor: Colors.blue,
-                            kIcon: Costomicons.vk,
-                            kText1: 'https://vk.com/mig_service22',
-                            kText2: ''),
-                        IconCont(kSize: kDesktopSize,
-                            kColor: Colors.green[500],
-                            kIcon: Costomicons.whatsapp,
-                            kText1: '+7-913-210-74-43',
-                            kText2: ''
-                        ),
-                        ]
-                    ),
-                  ),
-              ),
-            Expanded(
-              flex: 2,
-              child:
-              Container(
-                color: kPrimaryColor.withOpacity(0.30),
-                child: Column(
-                  children: [
-                    Text('г. Барнаул,', style: TextStyle(color: kTextColor, fontSize: kDesktopSize * 5 ),),
-                    Text('пр. Красоармейский 4', style: TextStyle(color: kTextColor, fontSize: kDesktopSize * 4 ),),
-                    Expanded(
-                      child: ListView(
-                        children:[
-                        Container(
-                          margin: EdgeInsets.all(kDefaultPadding),
-                          child: Row(
-                          mainAxisAlignment:  MainAxisAlignment.spaceAround,
-                          children: [
-                            Container(
-                              height: kDesktopSize * 30,
-                              width: kDesktopSize * 25,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
-                                  image:  DecorationImage(
-                                    alignment: AlignmentDirectional.center,
-                                    fit: BoxFit.cover,
-                                    image: AssetImage('assets/1.jpg')
-                                  ),
-                                ),),
-                            Container(
-                              height: kDesktopSize * 30,
-                              width: kDesktopSize * 25,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
-                                image:  DecorationImage(
-                                    alignment: AlignmentDirectional.center,
-                                    fit: BoxFit.cover,
-                                    image: AssetImage('assets/Coms4.jpg')
-                                ),
-                              ),),
-                            Container(
-                              height: kDesktopSize * 30,
-                              width: kDesktopSize * 25,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
-                                image:  DecorationImage(
-                                    alignment: AlignmentDirectional.center,
-                                    fit: BoxFit.cover,
-                                    image: AssetImage('assets/2.jpg')
-                                ),
-                              ),),
-                          ]
-                        ),),
-                          Container(
-                            height: kDesktopSize * 100,
-                           padding: EdgeInsets.all(kDefaultPadding),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
-                              image:  DecorationImage(
-                                  alignment: AlignmentDirectional.center,
-                                  fit: BoxFit.cover,
-                                  image: AssetImage('assets/Map.jpg')
-
-
-                              ),
-                            ),),
-                      ]
-                    ),
-                    ),
-                  ],
-                ),
-
-              )
-
-          ),
-            ],
-      ), ),
-
-      ]),
+            Responsive(
+              mobile: ContInformMob(kSize: kMobilSize),
+              tablet: ContInform(kSize: kTabletSize),
+              desktop: ContInform(kSize: kDesktopSize),
+            ),
+      ]
+      )
     );
   }
 }
+
 
 class OperatingMode extends StatelessWidget {
   const OperatingMode ({
@@ -179,41 +76,41 @@ class OperatingMode extends StatelessWidget {
                  Text('Режим работы:',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 5),),
+                       fontSize: kSize * 4),),
                  Text('Пн. 10:00-19:00',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Вт. 10:00-19:00',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Ср. 10:00-19:00',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Чт. 10:00-19:00',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Пт. 10:00-19:00',
                    style: TextStyle(
                        color: kTextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Сб. 10:00-19:00*',
                    style: TextStyle(
                        color: kRextColor,
-                       fontSize: kSize * 4),),
+                       fontSize: kSize * 3),),
                  Text('Вб. 10:00-19:00*',style: TextStyle(
                      color: kRextColor,
-                     fontSize: kSize * 4),),
+                     fontSize: kSize * 3),),
                  Container(
                    margin: EdgeInsets.all(kDefaultPadding),
                    child:
                    Text('  *В субботу и воскресенье сервисный центр работает в режиме приема и выдачи.',
                      style: TextStyle(
                          color: kRextColor,
-                         fontSize: kSize * 3),),
+                         fontSize: kSize * 2),),
                  ),
                ]
            ),
@@ -230,20 +127,16 @@ class IconCont extends StatelessWidget {
     this.kSize,
     this.kColor,
     this.kIcon,
-    this.kText1,
-    this.kText2,
+
   }) : super(key: key);
   final double kSize;
   final kColor;
   final kIcon;
-  final kText1;
-  final kText2;
+
 
 @override
 Widget build(BuildContext context) {
-return Row(
-  mainAxisAlignment: MainAxisAlignment.start,
-  children: [
+return
     Container(
       margin: EdgeInsets.all(kDefaultPadding),
       height: kSize * 8,
@@ -263,17 +156,238 @@ return Row(
           color: kTextColor,
         ),
       ),
-    ),
-    Column(
-      children: [
-        Text(kText1, style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),
-        Text(kText2, style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),
-      ],
-    ),
-  ],
-);
+    );
+
 }
 }
 
+class ContInform extends StatelessWidget {
+  const ContInform({
+    Key key,
+    this.kSize,
+  }) : super(key: key);
+  final double kSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child:Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child:
+            Container(
+              color: kPrimaryColor.withOpacity(0.30),
+              child:
+              ListView(
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                    OperatingMode(kSize: kSize,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconCont(
+                          kSize: kSize,
+                          kColor: Colors.green[500],
+                          kIcon: Icons.phone,),
+                        Column(
+                            children: [
+                              Text('+7(3852)-210-74-43', style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),
+                              Text('+7-913-210-74-43', style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),
+                            ]
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [IconCont(
+                        kSize: kSize,
+                        kColor: Colors.blue,
+                        kIcon: Costomicons.vk,
+                      ),
+                        Text('vk.com/mig_service22', style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),]),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [IconCont(
+                        kSize: kSize,
+                        kColor: Colors.green[500],
+                        kIcon: Costomicons.whatsapp,
+                      ),
+                        Text('+7-913-210-74-43', style: TextStyle(color: kTextColor, fontSize:kDesktopSize * 2.5),),
+                      ]),
+
+                  ]
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 2,
+              child:
+              Container(
+                color: kPrimaryColor.withOpacity(0.30),
+                child: Column(
+                  children: [
+                    Text('г. Барнаул,', style: TextStyle(color: kTextColor, fontSize: kSize * 5 ),),
+                    Text('пр. Красоармейский 4', style: TextStyle(color: kTextColor, fontSize: kSize * 4 ),),
+                    Expanded(
+                      child: ListView(
+                          children:[
+                            Container(
+                              margin: EdgeInsets.all(kDefaultPadding),
+                              child: Row(
+                                  mainAxisAlignment:  MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Container(
+                                      height: kSize * 30,
+                                      width: kSize * 25,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+                                        image:  DecorationImage(
+                                            alignment: AlignmentDirectional.center,
+                                            fit: BoxFit.cover,
+                                            image: AssetImage('assets/1.jpg')
+                                        ),
+                                      ),),
+                                    Container(
+                                      height: kSize * 30,
+                                      width: kSize * 25,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+                                        image:  DecorationImage(
+                                            alignment: AlignmentDirectional.center,
+                                            fit: BoxFit.cover,
+                                            image: AssetImage('assets/Coms4.jpg')
+                                        ),
+                                      ),),
+                                    Container(
+                                      height: kSize * 30,
+                                      width: kSize * 25,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+                                        image:  DecorationImage(
+                                            alignment: AlignmentDirectional.center,
+                                            fit: BoxFit.cover,
+                                            image: AssetImage('assets/2.jpg')
+                                        ),
+                                      ),),
+                                  ]
+                              ),),
+                            Container(
+                              margin: EdgeInsets.all(kDefaultPadding * 2),
+                              height: kSize * 100,
+
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+                                image:  DecorationImage(
+                                    alignment: AlignmentDirectional.center,
+                                    fit: BoxFit.cover,
+                                    image: AssetImage('assets/Map.jpg')
+                                ),
+                              ),),
+                          ]
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          ),
+        ],
+      ), );
+  }
+}
+
+class ContInformMob extends StatelessWidget {
+  const ContInformMob({
+    Key key,
+    this.kSize,
+  }) : super(key: key);
+  final double kSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+          Expanded(
+            child:
+
+            Container(
+              color: kPrimaryColor.withOpacity(0.30),
+              child:
+              ListView(
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                  Text('г. Барнаул,', style: TextStyle(color: kTextColor, fontSize: kSize * 5 ),),
+    Text('пр. Красоармейский 4', style: TextStyle(color: kTextColor, fontSize: kSize * 4 ),),
+                    OperatingMode(kSize: kSize,),
+                    IconCont(
+                        kSize: kSize,
+                        kColor: Colors.green[500],
+                        kIcon: Icons.phone
+                    ),
+                    IconCont(
+                        kSize: kSize,
+                        kColor: Colors.blue,
+                        kIcon: Costomicons.vk
+                    ),
+                    IconCont(kSize: kSize,
+                        kColor: Colors.green[500],
+                        kIcon: Costomicons.whatsapp
+                    ),
+                        Container(
+                        margin: EdgeInsets.all(kDefaultPadding),
+    child: Row(
+    mainAxisAlignment:  MainAxisAlignment.spaceAround,
+    children: [
+    Container(
+    height: kSize * 30,
+    width: kSize * 25,
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+    image:  DecorationImage(
+    alignment: AlignmentDirectional.center,
+    fit: BoxFit.cover,
+    image: AssetImage('assets/1.jpg')
+    ),
+    ),),
+    Container(
+    height: kSize * 30,
+    width: kSize * 25,
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+    image:  DecorationImage(
+    alignment: AlignmentDirectional.center,
+    fit: BoxFit.cover,
+    image: AssetImage('assets/Coms4.jpg')
+    ),
+    ),),
+    Container(
+    height: kSize * 30,
+    width: kSize * 25,
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+    image:  DecorationImage(
+    alignment: AlignmentDirectional.center,
+    fit: BoxFit.cover,
+    image: AssetImage('assets/2.jpg')
+    ),
+    ),),
+    ]
+    ),),
+    Container(
+    height: kSize * 100,
+    padding: EdgeInsets.all(kDefaultPadding),
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(kDefaultRadius  )),
+    image:  DecorationImage(
+    alignment: AlignmentDirectional.center,
+    fit: BoxFit.cover,
+    image: AssetImage('assets/Map.jpg')
+    ),
+    ),),
+                  ]
+              ),
+            ),
+          );
 
 
+  }
+}
