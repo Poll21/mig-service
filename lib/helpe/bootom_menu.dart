a619041397f73b7112bc360275ab60e615f99349
Poll <88119338+Poll21@users.noreply.github.com>
2022-03-21 08:57:41 +0700

    smallMenu
    доработать всплывающее меню

--- lib/helpe/bootom_menu.dart
+++ lib/helpe/bootom_menu.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:migservice/components/attention_page.dart';
 import 'package:migservice/components/prices/body_prise.dart';
 import 'package:migservice/components/prices/price_list.dart';
 import 'package:migservice/controllers/counterController.dart';
-import 'package:migservice/helpe/smol_bootom_menu.dart';
 import '../components/constants.dart';
 import '../pages/about_us.dart';
 import '../pages/home_screen.dart';
@@ -23,6 +21,120 @@ class MenuString {
   });
 }
 
+class BootomMenuSmall extends StatelessWidget {
+  final List<MenuString> botoonSmallData = [
+    MenuString(text: 'Смартфоны',
+      function: () {
+        Get.offAll(BodyPrice(
+            price: 'СМАРТФОНЫ', allPrices: smartPrice));
+      },
+    ),
+    MenuString(text: 'Планшеты',
+      function: () {
+        Get.to(BodyPrice(
+            price: 'Планшеты', allPrices: tabPrice));
+      },
+    ),
+    MenuString(text: 'Планшеты',
+      function: () {
+        Get.to(BodyPrice(
+            price: 'Планшеты', allPrices: tabPrice));
+      },),
+    MenuString(text: 'Планшеты',
+      function: () {
+        Get.to(BodyPrice(
+            price: 'Планшеты', allPrices: tabPrice));
+      },),
+    MenuString(text: 'Планшеты',
+      function: () {
+        Get.to(BodyPrice(
+            price: 'Планшеты', allPrices: tabPrice));
+      },),
+    MenuString(text: 'Планшеты',
+      function: () {
+        Get.to(BodyPrice(
+            price: 'Планшеты', allPrices: tabPrice));
+      },),
+  ];
+  final double kSize;
+
+  BootomMenuSmall({Key key, this.kSize}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size .height;
+    final List<Widget> botoonSmall =
+    botoonSmallData.map((MenuString menuString) =>
+        ButtonMenuItem(
+          text: menuString.text, function: menuString.function, kSize: kSize,))
+        .toList();
+    return CustomSingleChildLayout(
+        delegate: MySingleChildLayoutDelegate(
+            width / 4,
+            height / 3),
+        child:
+        AlertDialog(
+          contentPadding: EdgeInsets.all(0),
+          // actionsOverflowDirection: VerticalDirection.down,
+          elevation: 0,
+          backgroundColor: kPrimaryColor.withOpacity(1),
+          title: Column(children: botoonSmall),
+        ));
+  }
+}
+
+class BootomMenuBig extends StatelessWidget {
+  final double kSize;
+
+  BootomMenuBig({Key key, this.kSize}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<MenuString> botoonBigData = [
+      MenuString(text: 'Главная',
+        function: () {
+          Get.to(HomeScreen());
+        },
+      ),
+      MenuString(text: 'Ремонт',
+          function: () {
+            showDialog(
+                barrierColor: kPrimaryColor.withOpacity(0),
+                context: context,
+                builder: (BuildContext dialogContext) {
+                  return BootomMenuSmall(kSize: kSize,);
+                }); //добавить функцию
+          }),
+      MenuString(text: 'Услуги',
+        function: () {
+          Get.to(ServiceScreen());
+        },),
+      MenuString(text: 'Контакты',
+        function: () {
+          Get.to(ContactInf());
+        },),
+      MenuString(text: 'О нас',
+        function: () {
+          Get.to(AboutUsScreen());
+        },),
+      MenuString(text: 'О нас',
+        function: () {
+          Get.to(AboutUsScreen());
+        },),
+    ];
+    final List<Widget> botoonBig =
+    botoonBigData.map((MenuString menuString) =>
+        ButtonMenuItem(
+          text: menuString.text, function: menuString.function, kSize: kSize,))
+        .toList();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: botoonBig,);
+  }
+}
+
+
 class ButtonMenu extends StatelessWidget {
   final CounterController counterController = Get.put(CounterController());
   final double kSize;
@@ -31,121 +143,18 @@ class ButtonMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-      ButtonMenuItem(
-        text: 'Главная',
-        function: () {
-          Get.to(HomeScreen());
-        },
-        kSize: kSize,
-      ),
-      ButtonMenuItem(
-        text: 'Ремонт',
-        function: () {
-          showDialog(
-            barrierColor: kPrimaryColor.withOpacity(0),
-            context: context,
-            builder: (BuildContext dialogContext) {
-              return CustomSingleChildLayout(
-                  delegate: MySingleChildLayoutDelegate(
-                      width / 4,
-                      height / 2 - kDefaultPadding * 12),
-                  child:
-                  AlertDialog(
-                    contentPadding: EdgeInsets.all(0),
-                    // actionsOverflowDirection: VerticalDirection.down,
-                    elevation: 0,
-                    backgroundColor: kPrimaryColor.withOpacity(1),
-                    title: Column(children:[
-                      ButtonMenuItem(
-                        text: 'Смартфоны',
-                        function: () {
-                          Get.offAll(BodyPrice(
-                              price: 'СМАРТФОНЫ', allPrices: smartPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                      ButtonMenuItem(
-                        text: 'Планшеты',
-                        function: () {
-                          Get.to(BodyPrice(
-                              price: 'Планшеты', allPrices: smartPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                      ButtonMenuItem(
-                        text: 'Планшеты',
-                        function: () {
-                          Get.to(BodyPrice(
-                              price: 'Планшеты', allPrices: tabPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                      ButtonMenuItem(
-                        text: 'Главная',
-                        function: () {
-                          Get.to(BodyPrice(
-                              price: 'СМАРТФОНЫ', allPrices: smartPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                      ButtonMenuItem(
-                        text: 'Главная',
-                        function: () {
-                          Get.to(BodyPrice(
-                              price: 'СМАРТФОНЫ', allPrices: smartPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                      ButtonMenuItem(
-                        text: 'Главная',
-                        function: () {
-                          Get.to(BodyPrice(
-                              price: 'СМАРТФОНЫ', allPrices: smartPrice));
-                        },
-                        kSize: kSize,
-                      ),
-                    ]),
-                  ));
-            },
-          );
-          // Navigator.push(context, PageRouteBuilder(
-          //    opaque: false,
-          //    pageBuilder: (BuildContext context, _, __) => SmolBootomMenu(kSize: kMobilSize,)));
-        },
-        kSize: kSize,
-      ),
-      ButtonMenuItem(
-        text: 'Услуги',
-        function: () {
-          Get.to(ServiceScreen());
-        },
-        kSize: kSize,
-      ),
-      ButtonMenuItem(
-        text: 'Контакты',
-        function: () {
-          Get.to(ContactInf());
-        },
-        kSize: kSize,
-      ),
-      ButtonMenuItem(
-        text: 'О нас',
-        function: () {
-          Get.to(AboutUsScreen());
-        },
-        kSize: kSize,
-      ),
-      ButtonMenuItem(
-        text: 'Мои ремонты',
-        function: () {
-          Get.to(MyServiceScreen());
-        },
-        kSize: kSize,
-      ),
-    ]);
+    final double width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    final double height = MediaQuery
+        .of(context)
+        .size
+        .height;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: BootomMenuBig(kSize: kSize)
+    );
   }
 }
 
@@ -165,31 +174,60 @@ class ButtonMenuItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return
-        Container(
-            width: kSize * 20.0,
+      Container(
+          width: kSize * 20.0,
+          decoration: BoxDecoration(
+              color: kPrimaryColor.withOpacity(0.5),
+              border: Border.all(color: Colors.black12, width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 5),
+                  blurRadius: 5,
+                  color: kShadowColor.withOpacity(0.10),
+                ),
+              ]),
+          child: TextButton(
+              onPressed: function,
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontWeight: FontWeight.w100,
+                  color: kTextColor,
+                  fontSize: kSize * 2.5,
+                ),
+              )))
 
-            decoration: BoxDecoration(
-                color: kPrimaryColor.withOpacity(0.5),
-                border: Border.all(color: Colors.black12, width: 1),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 5),
-                    blurRadius: 5,
-                    color: kShadowColor.withOpacity(0.10),
-                  ),
-                ]),
-            child: TextButton(
-                onPressed: function,
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w100,
-                    color: kTextColor,
-                    fontSize: kSize * 2.5,
-                  ),
-                )))
+    ;
+  }
+}
 
-    );
+
+//задает положение расширяемой кнопки "Ремонт"
+
+class LocationBootons extends StatelessWidget {
+  const LocationBootons({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+class MySingleChildLayoutDelegate extends SingleChildLayoutDelegate {
+  MySingleChildLayoutDelegate(this.width, this.height);
+
+  final double width;
+  final double height;
+
+  @override
+  Offset getPositionForChild(Size size, Size childSize) {
+    return Offset(-width, -height);
+  }
+
+  @override
+  bool shouldRelayout(covariant SingleChildLayoutDelegate) {
+    return true;
   }
 }
