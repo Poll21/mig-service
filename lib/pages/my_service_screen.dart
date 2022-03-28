c545bfabbffb644321a8ba6634a55e5bbb74a21f
Poll21 <gol.poll21@gmail.com>
2022-03-28 15:21:50 +0700

    ffff

--- lib/pages/my_service_screen.dart
+++ lib/pages/my_service_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../components/helpe/costum_appbar.dart';
@@ -18,7 +20,36 @@ class MyServiceScreen extends StatelessWidget {
           CustomAppBarOthe(),
 
 
+
         ],
       ),);
   }
 }
+
+class App extends StatelessWidget {
+ final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(future: _initialization, builder: (context, snapshot){
+      if(snapshot.hasError){
+        return Text(snapshot.error.toString());
+      }
+      if(snapshot.connectionState == ConnectionState.done) {
+        return MyAwesomeApp();
+      }
+      return CircularProgressIndicator();
+    });
+  }
+}
+
+class MyAwesomeApp extends StatelessWidget{
+  const MyAwesomeApp ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
